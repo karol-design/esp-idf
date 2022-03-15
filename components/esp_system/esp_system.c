f0fcf9d953697084380c86911bb5965240dccdd9
Karol Wojslaw <wojslaw.tech@gmail.com>
2022-03-15 14:33:15 +0000

    Revert "patch"
    This reverts commit f89163941149ae776c4082bad1e704a11825f093.

--- components/esp_system/esp_system.c
+++ components/esp_system/esp_system.c
@@ -82,7 +82,7 @@ void IRAM_ATTR esp_restart(void)
     // Disable scheduler on this core.
     vTaskSuspendAll();
 
-    bool digital_reset_needed = true;
+    bool digital_reset_needed = false;
 #if CONFIG_ESP_SYSTEM_MEMPROT_FEATURE
 #if CONFIG_IDF_TARGET_ESP32S2
     if (esp_memprot_is_intr_ena_any() || esp_memprot_is_locked_any()) {
